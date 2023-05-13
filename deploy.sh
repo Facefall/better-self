947abd25bea3874fb8171532153caad6a00873bd
down <3108999582@qq.com>
2023-05-13 13:41:34 +0800

    chore: deploy sh change git url

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Facefall/better-self.git master:gh-pages
+git push -f https://Facefall.github.io/better-self main:gh-pages
 
 cd -
